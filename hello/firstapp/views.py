06fd4a53397219fa79aae89503c1e87cad5ee6dc
Дмитрий Сысоев <jdmitrys@yandex.ru>
2021-11-23 12:17:52 +0400

    URL & Views

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Django')
+    return HttpResponse('<h1>Main</h1>')
+
+def about(request):
+    return HttpResponse("<h1>About</h1>")
+
+def contact(request):
+    return HttpResponse("<h1>Contact</h1>")
